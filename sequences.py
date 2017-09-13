8ba2e626fc5b9d4d97661344368f41ca5c27b38e
lio100 <31461910+lio100@users.noreply.github.com>
2017-09-12 21:26:30 -0700

    Update sequences.py

--- sequences.py
+++ sequences.py
@@ -1,11 +1,15 @@
 def fibonacci(n):
     if(n <= 1):
         return n
+    #error when negative
     else:
         return(fibonacci(n-1) + fibonacci(n-2))
-n = int(input("Enter number of terms:"))
-my_list = []
-for i in range(n):
-    x = fibonacci(i)
-    my_list.append(x)
-print(my_list)
+    my_list = []
+    for i in range(n):
+        x = fibonacci(i)
+        my_list.append(x)
+    print(my_list)
+    
+    
+    
+#n = int(input("Enter number of terms:"))
