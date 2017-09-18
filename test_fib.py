24f054755b2e98acceb871df671295154b15eb1d
yaghm101 <31462111+yaghm101@users.noreply.github.com>
2017-09-18 13:17:38 -0700

    Update test_fib.py

--- test_fib.py
+++ test_fib.py
@@ -1,19 +1,6 @@
-import sequences
+import fib
 
-def test_1():
-  my_list=fibonacci(1);
-  result=my_list[-1];
-  correct=0;
-  assert result == correct;
-  
-def test_2():
-  my_list=fibonacci(2);
-  result=my_list[-1];
-  correct=1;
-  assert result == correct;
-
-def test_3():
-  my_list=fibonacci(10);
-  result=my_list[-1];
-  correct=30;
-  assert result == correct;
+def test_fib():
+    result  = fib.main(10)
+    correct = 55
+    assert result == correct
