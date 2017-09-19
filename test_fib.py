4aec99efa835523e1bc61fb0fecc90fcf5d640de
lio100 <31461910+lio100@users.noreply.github.com>
2017-09-18 21:44:59 -0700

    Update test_fib.py

--- test_fib.py
+++ test_fib.py
@@ -1,10 +1,9 @@
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import fib
 
 def test_fib():
-    result =fib.main(5);
-    correct=5;
-    assert result == correct;
+    result=fib.main(5)
+    correct=5
+    assert result == correct
