ae392232e48bae17c4e9c3e2151e52067e08afca
lio100 <31461910+lio100@users.noreply.github.com>
2017-09-19 07:47:33 -0700

    Update klrunfibs.sh

--- klrunfibs.sh
+++ klrunfibs.sh
@@ -11,34 +11,50 @@
                 #do not overwrite file
                 #exit program
 
-if [ $1 -le 100000 && $1 -ge 1 ]
-then
-    return 0
-else
-    echo $1 Error:invalid input. Enter integer from 1 to 10,000.
-    #ask for input again
-    return 1
-fi
-
-
-for i in {1..$1}
-do
-    python fib.py $1 >> fibs.csv
+################ main code ################
+while [  valid_input($1) ]; do
+    csv_exists
 done
 
 
 
+################ write to fibs.csv ################
+write_fibs.csv () {
+    for i in {1..$1}
+    do
+        python fib.py $1 >> fibs.csv
+    done
+}
 
-fib.py
+write_fibs.csv.bak () {
+    for i in {1..$1}
+    do
+        cp fib.csv fib.csv.bak
+        python fib.py $1 >> fibs.csv
+    done
+}
+
+################ argument 1:10000 ################
+##################################################
+valid_input ($1) {
+    if [ $1 -le 100000 && $1 -ge 1 ]
+    then
+        return 0
+    else
+        echo $1 Error:invalid input. Enter integer from 1 to 10,000.
+        #ask for input again?
+        return 1
+    fi
+}
 
 ################ checks if fibs.csv & fibs.csv.bak exists ################
 csv_exists () {       
     if [ -e fibs.csv ]                  
     then
-        bak_exists 
+        bak_exists
     else
         #fibs.csv does not exist
-        #write to fibs.csv
+        write_fibs.csv
         return 0
     fi
 }
@@ -47,12 +63,13 @@ csv_exists () {
 bak_exists () {
     if [ -e fibs.csv.bak ]              
     then                            
-        echo fibs.csv and fibs.csv.bak exists. output not saved to file.
+        echo Error: fibs.csv and fibs.csv.bak exists. Output not saved to file.
         return 1
         exit 1
     else 
-        echo existing fibs.csv saved as fibs.csv.bak \nfibs.csv overwritten
-        mv fibs.csv fibs.csv.bak
+        echo Existing fibs.csv saved as fibs.csv.bak \nfibs.csv overwritten
+        cp fibs.csv fibs.csv.bak
+        write_fibs.csv
         return 0
     fi
 }
