60617342863916dc27e1e25f4effd1cffc8a365c
lio100 <31461910+lio100@users.noreply.github.com>
2017-09-19 08:10:53 -0700

    Update kl.sh

--- kl.sh
+++ kl.sh
@@ -13,69 +13,14 @@
 
 
 
-################ main code ################
-if [  valid_input $1 ]
-then
-    csv_exists
-    echo thenstmt
-else
-    echo elsestmt
-fi
 
 
-
-################ write to fibs.csv ################
-write_fibs.csv () {
-    for i in {1..$1}
-    do
-        python ./fib.py $1 >> fibs.csv
-    done
-}
-
-write_fibs.csv.bak () {
-    for i in {1..$1}
-    do
-        cp fib.csv fib.csv.bak
-        python fib.py $1 >> fibs.csv
-    done
-}
-
-################ argument 1:10000 ################
-##################################################
-valid_input () {
     if [ $1 -le 100000 ] && [ $1 -ge 1 ]
     then
+        ech valid input
         return 0
     else
         echo $1 Error:invalid input. Enter integer from 1 to 10,000.
         #ask for input again?
         return 1
     fi
-}
-
-################ checks if fibs.csv & fibs.csv.bak exists ################
-csv_exists () {       
-    if [ -e fibs.csv ]                  
-    then
-        bak_exists
-    else
-        #fibs.csv does not exist
-        write_fibs.csv
-        return 0
-    fi
-}
-
-################ checks if fibs.csv.bak exists ################
-bak_exists () {
-    if [ -e fibs.csv.bak ]              
-    then                            
-        echo Error: fibs.csv and fibs.csv.bak exists. Output not saved to file.
-        return 1
-        exit 1
-    else 
-        echo Existing fibs.csv saved as fibs.csv.bak \nfibs.csv overwritten
-        cp fibs.csv fibs.csv.bak
-        write_fibs.csv
-        return 0
-    fi
-}
