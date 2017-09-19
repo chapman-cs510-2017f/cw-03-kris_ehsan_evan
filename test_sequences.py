d8faf130ff015baf3269a3453f952286771c3dfb
lio100 <31461910+lio100@users.noreply.github.com>
2017-09-18 20:23:47 -0700

    Update test_sequences.py

--- test_sequences.py
+++ test_sequences.py
@@ -1,19 +1,19 @@
-import sequences as seq
+import sequences
 
 def test_1():
-  my_list=seq.fibonacci(1);
+  my_list=sequences.fibonacci(1);
   result=my_list[-1];
   correct=0;
   assert result == correct;
   
 def test_2():
-  my_list=seq.fibonacci(2);
+  my_list=sequences.fibonacci(2);
   result=my_list[-1];
   correct=1;
   assert result == correct;
 
 def test_3():
-  my_list=seq.fibonacci(10);
+  my_list=sequences.fibonacci(10);
   result=my_list[-1];
   correct=30;
   assert result == correct;
