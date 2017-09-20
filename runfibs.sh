41e62d0e7089bb8dbf07385179b6591a6e28be82
Kristalee Lio <lio100@mail.chapman.edu>
2017-09-20 04:59:56 +0000

    KL

--- runfibs.sh
+++ runfibs.sh
@@ -6,29 +6,18 @@ then
     then
         echo "Error: fibs.csv and fibs.csv.bak exists. Output not saved to file."
         exit 1
-    else
-        echo "Existing fibs.csv saved as fibs.csv.bak. fibs.csv overwritten."
-        cp fibs.csv fibs.csv.bak
-        for i in $(seq $1);
-        do
-            echo -n "$(./fib.py $i)," >> fibs.csv
-        done
-    fi
+    else
+        echo "Existing fibs.csv saved as fibs.csv.bak. fibs.csv overwritten."
+        cp fibs.csv fibs.csv.bak
+        for i in $(seq $1);
+        do
+            echo -n "$(./fib.py $i)," >> fibs.csv
+        done
+    fi
 else
     for i in $(seq $1);
     do
         echo -n "$(./fib.py $i)," >> fibs.csv
     done
 fi
-
-#touch  "fibs.csv"
- 
-#for i in $(seq $1);
-#do
-#    echo -n "$(./fib.py $i)," >> fibs.csv
-#done
-
-
-
-
-
+#lkdj
