bb22a55ba119392a60400c0c2b060357cb88334f
lio100 <31461910+lio100@users.noreply.github.com>
2017-09-19 20:14:04 -0700

    Update runfibs.sh

--- runfibs.sh
+++ runfibs.sh
@@ -1,42 +1,37 @@
 #!/bin/bash
-
-
-
-if [ -f fibs.csv ]                  
+ 
+if [ -e "fibs.csv" ]
 then
-    if [ -f fibs.csv.bak ]              
-    then                            
-        echo Error: fibs.csv and fibs.csv.bak exists. Output not saved to file.
-        exit 1
-    else 
-        echo Existing fibs.csv saved as fibs.csv.bak. fibs.csv overwritten
-        cp fibs.csv fibs.csv.bak
-        for i in $(seq $1);
-        do
-            echo -n "$(./fib.py $i)," >> fibs.csv
-        done
-    fi        
+    if [ -e "fibs.csv.bak" ]
+    then
+        echo Error: fibs.csv and fibs.csv.bak exists. Output not saved to file.
+        exit 1
+    else
+        echo Existing fibs.csv saved as fibs.csv.bak. fibs.csv overwritten
+        cp fibs.csv fibs.csv.bak
+        for i in $(seq $1);
+        do
+            echo -n "$(./fib.py $i)," >> fibs.csv
+        done
+    fi
 else
-    echo else
-    exit 0
+    for i in $(seq $1);
+    do
+            echo -n "$(./fib.py $i)," >> fibs.csv
+    done
 fi
-
-
-
-
-
-
-#touch  "fibs.csv"
-
-
-
-
-
-#works
-#for i in $(seq $1);
+ 
+ 
+ 
+ 
+#touch  "fibs.csv"
+ 
+#for i in $(seq $1);
 #do
-#    echo -n "$(./fib.py $i)," >> fibs.csv
+#    echo -n "$(./fib.py $i)," >> fibs.csv
 #done
+
+
 
 
 
